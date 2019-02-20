e568dfbb1b6b8e8f394877412cb0c1576c1a71fe
car <carmelogustavo@gmail.com>
2019-02-20 14:45:35 +0100

    new checks

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { User } = require('../models');
 
@@ -8,8 +9,17 @@ exports.createUser = async ctx => {
     const email = ctx.request.body.email.toLowerCase();
     const password = await bcrypt.hash(ctx.request.body.password, 10);
     const newUser = new User({ name, email, password });
+    console.log(process.env.APP_SECRET);
     newUser.save();
     ctx.status = 201;
+    const token = jwt.sign({ userId: newUser._id }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    console.log('HEY');
   } catch (e) {
     ctx.status = 400;
     ctx.body = {
